24c240622f996ac0705630fac8d3bcfd9f1d2e73
Angel D. Munoz <scythes.lol@gmail.com>
2020-11-27 13:27:16 -0700

    provide an objectid converter

--- src/Mondocks/Library.fs
+++ src/Mondocks/Library.fs
@@ -4,12 +4,24 @@ type IBuilder =
     abstract ToJSON: unit -> string
 
 module internal Json =
+    open System
     open System.Text.Json
     open System.Text.Json.Serialization
+    open MongoDB.Bson
+
+    type ObjectIdConverter() =
+        inherit JsonConverter<ObjectId>()
+
+        override _.Read(reader: byref<Utf8JsonReader>, typeToConvert: Type, options: JsonSerializerOptions) =
+            ObjectId.Parse(reader.GetString())
+
+        override _.Write(writer: Utf8JsonWriter, value: ObjectId, options: JsonSerializerOptions) =
+            writer.WriteStringValue(value.ToString())
 
     let private defaults =
         let options = JsonSerializerOptions()
         options.Converters.Add(JsonFSharpConverter())
+        options.Converters.Add(ObjectIdConverter())
         options.IgnoreNullValues <- true
         options
 
