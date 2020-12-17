e508c0ff67c8c92aeb7b0f8c0a54252e7947c3f4
Angel D. Munoz <scythes.lol@gmail.com>
2020-12-17 10:57:53 -0700

    docs add doc comments

--- src/Mondocks/Aggregation.fs
+++ src/Mondocks/Aggregation.fs
@@ -8,6 +8,11 @@ module Aggregation =
 
     [<AutoOpen>]
     module Count =
+
+        /// <summary>
+        /// Represents a `Count` command
+        /// for more information take a look at <a href="https://docs.mongodb.com/manual/reference/command/count/#syntax">count syntax</a>
+        /// </summary>
         type CountCommand<'TDocument, 'Hint, 'ReadConcern, 'Comment> =
             { count: string
               query: Option<'TDocument>
@@ -36,18 +41,61 @@ module Aggregation =
 
             member __.Run(state: CountCommand<'TDocument, 'Hint, 'ReadConcern, 'Comment>) = (state :> IBuilder).ToJSON()
 
+            ///<summary>The name of the collection  to query against</summary>
+            /// <example>
+            ///    count {
+            ///        collection "users"
+            ///        query {| age = 50 |}
+            ///    }
+            /// </example>
             [<CustomOperation("collection")>]
             member __.Count(state: CountCommand<'TDocument, 'Hint, 'ReadConcern, 'Comment>, count: string) =
                 { state with count = count }
 
+            /// <summary>
+            ///  Optional object that is going to be used to filter the find query
+            /// </summary>
+            /// <example>
+            ///     count {
+            ///       collection "users"
+            ///       // counts all the users with the name "Frank"
+            ///       query {| name = "Frank"|}
+            ///     }
+            /// </example>
             [<CustomOperation("query")>]
             member __.Query(state: CountCommand<'TDocument, 'Hint, 'ReadConcern, 'Comment>, query: 'TDocument) =
                 { state with query = Some query }
 
+            /// <summary>
+            /// Optional. An integer value used to omit the first n amount of documents from the find query
+            /// Use together with Limit to paginate results
+            /// </summary>
+            /// <example>
+            ///     count {
+            ///       collection "users"
+            ///       // ... omit other fields
+            ///       // omits the first 10 results of the count query
+            ///       skip 10
+            ///       // ... omit other fields
+            ///     }
+            /// </example>
             [<CustomOperation("limit")>]
             member __.Limit(state: CountCommand<'TDocument, 'Hint, 'ReadConcern, 'Comment>, limit: int) =
                 { state with limit = Some limit }
 
+            /// <summary>
+            /// Optional. An integer value used to take at most n amount of documents
+            /// Use together with Skip to paginate results
+            /// </summary>
+            /// <example>
+            ///     count {
+            ///       collection "users"
+            ///       // ... omit other fields
+            ///       // takes only 10 documents from the count query
+            ///       limit 10
+            ///       // ... omit other fields
+            ///     }
+            /// </example>
             [<CustomOperation("skip")>]
             member __.Skip(state: CountCommand<'TDocument, 'Hint, 'ReadConcern, 'Comment>, skip: int) =
                 { state with skip = Some skip }
@@ -65,11 +113,19 @@ module Aggregation =
             member __.Comment(state: CountCommand<'TDocument, 'Hint, 'ReadConcern, 'Comment>, comment: 'Comment) =
                 { state with comment = Some comment }
 
+        /// <summary>Creates an count command for documents in the specified collection</summary>
+        /// <param name="collection">The name of the collection to perform this query against</param>
+        /// <returns>returns a <see cref="Mondocks.Aggregation.Count.CountCommandBuilder">CountCommandBuilder</see></returns>
         let count = CountCommandBuilder()
 
 
     [<AutoOpen>]
     module Distinct =
+
+        /// <summary>
+        /// Represents a `Distinct` command
+        /// for more information take a look at <a href="https://docs.mongodb.com/manual/reference/command/distinct/#syntax">count syntax</a>
+        /// </summary>
         type DistinctCommand<'TDocument, 'ReadConcern, 'Comment> =
             { distinct: string
               key: string
@@ -95,10 +151,28 @@ module Aggregation =
                 ({ state with distinct = collection } :> IBuilder)
                     .ToJSON()
 
+            /// <summary>
+            /// A key used to retreive values from the query result documents
+            /// </summary>
+            /// <example>
+            ///     distinct "users" {
+            ///       key "email"
+            ///     }
+            /// </example>
             [<CustomOperation("key")>]
             member __.Key(state: DistinctCommand<'TDocument, 'ReadConcern, 'Comment>, key: string) =
                 { state with key = key }
 
+            /// <summary>
+            /// Optional. A filter to query against the collection
+            /// </summary>
+            /// <example>
+            ///     distinct "users" {
+            ///       // gets all of the distinc emails from users named "Frank"
+            ///       key "email"
+            ///       query {| name = "Frank"|}
+            ///     }
+            /// </example>
             [<CustomOperation("query")>]
             member __.Query(state: DistinctCommand<'TDocument, 'ReadConcern, 'Comment>, query: 'TDocument) =
                 { state with query = Some query }
@@ -117,4 +191,7 @@ module Aggregation =
             member __.Comment(state: DistinctCommand<'TDocument, 'ReadConcern, 'Comment>, comment: 'Comment) =
                 { state with comment = Some comment }
 
+        /// <summary>Creates an update command for documents in the specified collection</summary>
+        /// <param name="collection">The name of the collection to perform this query against</param>
+        /// <returns>returns a <see cref="Mondocks.Aggregation.Distinct.DistinctCommandBuilder">DistinctCommandBuilder</see></returns>
         let distinct (collection: string) = DistinctCommandBuilder(collection)
