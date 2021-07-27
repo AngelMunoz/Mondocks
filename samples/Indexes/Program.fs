9ac04c948ec08aa7de6cfb99621b3c852c89d501
Angel D. Munoz <scythes.lol@gmail.com>
2021-07-27 16:44:45 -0500

    housekeeping: update samples deps

--- samples/Indexes/Program.fs
+++ samples/Indexes/Program.fs
@@ -6,52 +6,55 @@ open Mondocks.Types
 
 
 let createIndexCmd =
-    let myindex = 
+    let myindex =
         index "user_email_unique_idx" {
-            key (dict(["email", box 1.0]))
+            key (dict ([ "email", box 1.0 ]))
             unique true
         }
-    let myindex2 = 
-        index "lastName_idx" {
-            key (dict(["lastName", box -1.0]))
-        }
-    let myindex3 = 
-        index "lastName2_idx" {
-            key (dict(["lastName2", box -1.0]))
-        }
-    let myindex4 = 
-        index "lastName3_idx" {
-            key (dict(["lastName3", box -1.0]))
-        }
-        
+
+    let myindex2 =
+        index "lastName_idx" { key (dict ([ "lastName", box -1.0 ])) }
+
+    let myindex3 =
+        index "lastName2_idx" { key (dict ([ "lastName2", box -1.0 ])) }
+
+    let myindex4 =
+        index "lastName3_idx" { key (dict ([ "lastName3", box -1.0 ])) }
+
     createIndexes "users" {
-        indexes [myindex; myindex2; myindex3; myindex4]
+        indexes [ myindex
+                  myindex2
+                  myindex3
+                  myindex4 ]
     }
 
-let dropIndexCmd = 
-    dropIndexes "users" {
-        index "lastName_idx"
-    }
+let dropIndexCmd =
+    dropIndexes "users" { index "lastName_idx" }
 
-let dropIndexesCmd = 
+let dropIndexesCmd =
     dropIndexes "users" {
-        index [
-            "lastName2_idx"
-            "lastName3_idx"
-        ]
+        index [ "lastName2_idx"
+                "lastName3_idx" ]
     }
 
 
 [<EntryPoint>]
 let main argv =
-    let client = MongoClient("mongodb://localhost:27017")
+    let client =
+        MongoClient("mongodb://192.168.100.5:27017")
+
     let db = client.GetDatabase("mondocks")
 
     printfn $"{createIndexCmd}\n{dropIndexCmd}"
 
-    let createIndexResult = db.RunCommand<CreateIndexResult>(JsonCommand createIndexCmd)
-    let dropIndexResult = db.RunCommand<DropIndexResult>(JsonCommand dropIndexCmd)
-    let dropIndexesResult = db.RunCommand<DropIndexResult>(JsonCommand dropIndexesCmd)
-    
+    let createIndexResult =
+        db.RunCommand<CreateIndexResult>(JsonCommand createIndexCmd)
+
+    let dropIndexResult =
+        db.RunCommand<DropIndexResult>(JsonCommand dropIndexCmd)
+
+    let dropIndexesResult =
+        db.RunCommand<DropIndexResult>(JsonCommand dropIndexesCmd)
+
     printfn $"%A{createIndexResult.ToJson()}\n{dropIndexResult.ToJson()}\n{dropIndexesResult.ToJson()}"
     0 // return an integer exit code// return an integer exit code
